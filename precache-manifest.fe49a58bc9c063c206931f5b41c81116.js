fe414f6e40561fd8b90a3c614530cd32c9b56efd
TUanphan nam <phantuanw.o.t@gmail.com>
2020-09-08 17:40:03 +0700

    haha

---
+++ precache-manifest.fe49a58bc9c063c206931f5b41c81116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72198901a071ba46eee52144ffdfe117",
+    "url": "/PhanTuanV1/index.html"
+  },
+  {
+    "revision": "d032644b1660ec1998e1",
+    "url": "/PhanTuanV1/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d032644b1660ec1998e1",
+    "url": "/PhanTuanV1/static/js/2.daa2d9d8.chunk.js"
+  },
+  {
+    "revision": "03c49537823378cb254d",
+    "url": "/PhanTuanV1/static/js/main.cf359365.chunk.js"
+  },
+  {
+    "revision": "dc2ab1cf650abdb313be",
+    "url": "/PhanTuanV1/static/js/runtime~main.54c490f7.js"
+  }
+]);
